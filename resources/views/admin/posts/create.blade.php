3753459abce2324dc10c3da5d99b2c38fa95f261
Dima Baraban <barabandima9@gmail.com>
2023-03-03 15:12:20 +0200

    Added connection of tags and posts
    modified:
    * Admin.PostController
    * Models.Post
    * config.filesystems
    * posts.create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -10,8 +10,8 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Главная</a></li>
-                        <li class="breadcrumb-item active">Статьи</li>
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Blank Page</li>
                     </ol>
                 </div>
             </div>
@@ -25,11 +25,12 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Создание статьи</h3>
+                            <h3 class="card-title">Новая статья</h3>
                         </div>
                         <!-- /.card-header -->
 
-                        <form role="form" method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+                        <form role="form" method="post" action="{{ route('posts.store') }}"
+                            enctype="multipart/form-data">
                             @csrf
                             <div class="card-body">
                                 <div class="form-group">
@@ -41,17 +42,20 @@
 
                                 <div class="form-group">
                                     <label for="description">Цитата</label>
-                                    <textarea name="description" class="form-control" id="description" rows="3" placeholder="Цитата ..."></textarea>
+                                    <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description"
+                                        rows="3" placeholder="Цитата ..."></textarea>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="content">Контент</label>
-                                    <textarea name="content" class="form-control" id="content" rows="5" placeholder="Контент ..."></textarea>
+                                    <textarea name="content" class="form-control @error('content') is-invalid @enderror" id="content" rows="7"
+                                        placeholder="Контент ..."></textarea>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="category_id">Категория</label>
-                                    <select class="form-control" id="category_id" name="category_id">
+                                    <select class="form-control @error('category_id') is-invalid @enderror" id="category_id"
+                                        name="category_id">
                                         @foreach ($categories as $k => $v)
                                             <option value="{{ $k }}">{{ $v }}</option>
                                         @endforeach
@@ -68,10 +72,6 @@
                                     </select>
                                 </div>
 
-
-
-
-
                                 <div class="form-group">
                                     <label for="thumbnail">Изображение</label>
                                     <div class="input-group">
@@ -81,11 +81,13 @@
                                         </div>
                                     </div>
                                 </div>
-                                <!-- /.card-body -->
 
-                                <div class="card-footer">
-                                    <button type="submit" class="btn btn-primary">Сохранить</button>
-                                </div>
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Сохранить</button>
+                            </div>
                         </form>
 
                     </div>
